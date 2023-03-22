a1a338fa9d95a25118843771cb5892e4461c4783
arlen <arlen-lt@protonmail.com>
2023-03-22 09:00:59 +0000

    add vscode_server_installer.sh

---
+++ vscode_server_installer.sh
@@ -0,0 +1,30 @@
+#!/bin/bash -e
+
+usage(){
+	echo "Usage: vscode_server_installer [options]"
+    echo "--version"
+	echo "--commit[=]<commit_hash>  install vscode server of specified commit hash"
+	exit 0
+}
+
+install(){
+    commit_hash=$1
+    rm -rf ~/.vscode-server/bin/${commit_hash} && mkdir -p ~/.vscode-server/bin/${commit_hash}
+    wget -O /tmp/vscode-server-stable-linux-x64.tar.gz https://vscode.cdn.azure.cn/stable/${commit_hash}/vscode-server-linux-x64.tar.gz \
+		&& tar -xzf /tmp/vscode-server-stable-linux-x64.tar.gz -C ~/.vscode-server/bin/${commit_hash} --strip-components 1
+}
+
+options=$(getopt -n "Installing VS Code Server" -l "help,version,commit:" -o "h" -a -- "$@")
+version=v0.0.1
+eval set -- ${options}
+while true
+do
+    case "$1" in
+    --commit) shift; install $1 ;;
+    -h | --help) usage ;;
+    --version) echo "vscode-server-installer $version by https://github.com/Arlen-LT/utility"; exit 0 ;;
+    --) shift; break;;
+	esac
+    shift
+done
+
